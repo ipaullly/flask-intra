ec281003959d059b28fdc29b9de14a1c07d81219
ipaully <ipaully22@gmail.com>
2018-09-17 23:26:49 +0300

    chore: Add postgresql environment variables

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class BaseConfig(object):
     SECRET_KEY = '\xc7Qi\xfd\xf1\x8a\x9c\x0b\x06\xc6'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
 
--- db_create.py
+++ db_create.py
@@ -7,5 +7,6 @@ db.create_all()
 #insert
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
+db.session.add(BlogPost("postgres", "we sey up a local postgres server"))
 #commit the changes
 db.session.commit()
